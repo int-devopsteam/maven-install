ad2e5e762ff26e034313296c3fc8bc46bccc7211
siddiquiintiquab <siddiquiintiquab@gmai.com>
2024-08-17 20:43:36 +0530

    infromation about to install java for maven

---
+++ java.sh
@@ -0,0 +1,11 @@
+in this there is a command that how to install java for maven ..
+
+java is a Pre Requisite Software to install maven ..
+
+to install Jdk which stands for ( java development kit )
+
+sudo yum install java-11-amazon-corretto-devel -y
+
+( to install java in Linux server for maven )
+
+	java -version is a command to check wethere java is install or not..
